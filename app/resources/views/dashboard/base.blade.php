c0304fb75b9d509caea9c8015767b1dbb89504b0
Dimitris Frangiadakis <dimitris@frangiadakis.com>
2022-05-26 01:41:52 +0200

    Adds sort of responsive menu

--- app/resources/views/dashboard/base.blade.php
+++ app/resources/views/dashboard/base.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/@popperjs/core@2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js" crossorigin="anonymous"></script>
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -15,6 +16,13 @@
         <div class="preloader flex-column justify-content-center align-items-center">
             <img class="animation__shake" src="{{asset('img/tesla.png')}}" alt="AdminLTELogo" height="60" width="60">
         </div>
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fa fa-bars"></i></a>
+                </li>
+            </ul>
+        </nav>
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <a href="#" class="brand-link">
                 <span class="brand-text font-weight-light">{{env('APP_NAME')}}</span>
@@ -39,7 +47,7 @@
             </div>
         </aside>
         <section class="content">
-            <div class="container-fluid" style="margin-left: 40px">
+            <div class="container-fluid" style="margin-left: 40px;text-align: center">
                 @yield('content')
             </div>
         </section>
